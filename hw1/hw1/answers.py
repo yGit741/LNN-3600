842ff968a564972d3ca45f3fb6feda895e5eb1d2
yGit741 <ygreenshpan@gmail.com>
2024-04-23 12:59:49 +0300

    last env fix.

---
+++ hw1/hw1/answers.py
@@ -0,0 +1,131 @@
+r"""
+Use this module to write your answers to the questions in the notebook.
+
+Note: Inside the answer strings you can use Markdown format and also LaTeX
+math (delimited with $$).
+"""
+
+student_name_1 = '' # string
+student_ID_1 = '' # string
+student_name_2 = '' # string
+student_ID_2 = '' # string
+
+
+# ==============
+# Part 1 answers
+
+part1_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part1_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+# ==============
+# Part 2 answers
+
+part2_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part2_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part2_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+# ==============
+
+# ==============
+# Part 3 answers
+
+part3_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+# ==============
+
+# ==============
